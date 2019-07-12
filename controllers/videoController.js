c360cc4b4435050536d20d71276389e41a8e3b28
yeonggwan <hjk781@gmail.com>
2019-07-13 05:55:48 +0900

    search,join,login,home html

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,23 @@
-export const home = (req, res) => res.render("home", { pageTitle: "홈" });
-export const search = (req, res) => res.render("search", { pageTitle: "검색" });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "홈", videos });
+};
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "검색", searchingBy });
+};
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "업로드" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "비디오" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "비디오 수정" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "비디오 삭제" });
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video one",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "yg",
+      email: "yg@hab.com"
+    }
+  },
+  {
+    id: 1212121,
+    title: "Video two",
+    description: "비디오 테스트",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "yg",
+      email: "yg@hab.com"
+    }
+  },
+  {
+    id: 55555,
+    title: "Video tree",
+    description: "비디오 테스트",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "yg",
+      email: "yg@hab.com"
+    }
+  },
+  {
+    id: 11111,
+    title: "Video four",
+    description: "비디오 테스트",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "yg",
+      email: "yg@hab.com"
+    }
+  }
+];
