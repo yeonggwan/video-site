50a02770beb2fdcaaec15415c1824c4729fbf7a0
yeonggwan <hjk781@gmail.com>
2019-07-13 02:34:23 +0900

    routers and controller

--- app.js
+++ app.js
@@ -3,22 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my page");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
