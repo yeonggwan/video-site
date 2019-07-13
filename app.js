2bd314335bb2c34fe71a5fc9f94ab295043212e2
yeonggwan <hjk781@gmail.com>
2019-07-14 02:01:40 +0900

    webpack

--- app.js
+++ app.js
@@ -8,14 +8,12 @@ import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from my page");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
