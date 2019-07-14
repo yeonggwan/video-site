c15161994721611730dc2e96501daa8b99a31ad8
yeonggwan <hjk781@gmail.com>
2019-07-14 09:27:48 +0900

    build

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,7 @@ export const postJoin = async (req, res, next) => {
     body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
+    req.flash("error", "비밀번호가 일치하지 않습니다");
     res.status(400);
     res.render("join", { pageTitle: "회원가입" });
   } else {
@@ -32,10 +33,15 @@ export const getLogin = (req, res) =>
 
 export const postLogin = passport.authenticate("local", {
   failureRedirect: routes.login,
-  successRedirect: routes.home
+  successRedirect: routes.home,
+  successFlash: "Welcome",
+  failureFlash: "이메일/비밀번호를 확인해주세요"
 });
 
-export const githubLogin = passport.authenticate("github");
+export const githubLogin = passport.authenticate("github", {
+  successFlash: "Welcome",
+  failureFlash: "로그인 할 수 없습니다"
+});
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
@@ -65,6 +71,7 @@ export const postGithubLogIn = (req, res) => {
 };
 
 export const logout = (req, res) => {
+  req.flash("info", "로그아웃 되었습니다");
   req.logout();
   res.redirect(routes.home);
 };
@@ -82,6 +89,7 @@ export const userDetail = async (req, res) => {
     console.log(user);
     res.render("userDetail", { pageTitle: "User Detail", user });
   } catch (error) {
+    req.flash("error", "유저를 찾을 수 없습니다");
     res.redirect(routes.home);
   }
 };
@@ -100,8 +108,10 @@ export const postEditProfile = async (req, res) => {
       email,
       avatarUrl: file ? file.location : req.user.avatarUrl
     });
+    req.flash("success", "프로필이 수정되었습니다");
     res.redirect(routes.me);
   } catch (error) {
+    req.flash("error", "프로필 수정에 실패했습니다");
     res.redirect(routes.editProfile);
   }
 };
@@ -115,6 +125,7 @@ export const postChangePassword = async (req, res) => {
   } = req;
   try {
     if (newPassword !== newPassword1) {
+      req.flash("error", "비밀번호가 일치하지 않습니다");
       res.status(400);
       res.redirect(`/users/${routes.changePassword}`);
       return;
@@ -122,6 +133,7 @@ export const postChangePassword = async (req, res) => {
     await req.user.changePassword(oldPassword, newPassword);
     res.redirect(routes.me);
   } catch (error) {
+    req.flash("error", "비밀번호를 변경하지 못했습니다");
     res.status(400);
     res.redirect(`/users/${routes.changePassword}`);
   }
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -71,7 +71,11 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    if (String(video.creator) !== req.user.id) {
+      throw Error();
+    } else {
+      res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    }
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -97,7 +101,7 @@ export const deleteVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    if (video.creator !== req.user.id) {
+    if (String(video.creator) !== req.user.id) {
       throw Error();
     } else {
       await Video.findOneAndRemove({ _id: id });
